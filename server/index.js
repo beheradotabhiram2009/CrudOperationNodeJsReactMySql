5f459f1db55997f2e8b505a23a2a58ffced0c668
Abhiram Behera <behera.abhiram2009@gmail.com>
2023-09-03 16:03:48 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const  { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+let con = {};
+const app = express();
+app.use(cors());
+
+const schema = buildSchema(`
+  type User {
+    id: Int
+    name: String
+    email: String
+    job_title: String
+  }
+  type Query {
+    getUsers: [User],
+    getUser(id: Int) : User
+  }
+  type Mutation {
+    updateUser(id: Int, name: String, email: String, job_title: String): Boolean
+    createUser(name: String, email: String, job_title: String): Boolean
+    deleteUser(id: Int): Boolean
+  }
+`);
+
+con = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'root',
+  database : 'userapp'
+});
+
+con.connect((err) => {
+  if (err) {
+    console.log('Error connecting to database:'+err);
+    return;
+  }
+  console.log('Connected to database');
+});
+
+app.use((req, res, next) => {
+  req.mysqlDb = con;
+  next();
+});
+
+const queryDB = (req, sql, args) => new Promise((resolve, reject) => {
+  req.mysqlDb.query(sql, args, (err, rows) => {
+      if (err) return reject(err);
+      rows.changedRows || rows.affectedRows || rows.insertId ? resolve(true) : resolve(rows);
+  });
+});  
+
+const root = {
+  getUsers: (args, req) => queryDB(req, "select * from users").then(data => data),
+  getUser: (args, req) => queryDB(req, "select * from users where id = ?", [args.id]).then(data => data[0]),
+  updateUser: (args, req) => queryDB(req, "update users SET ? where id = ?", [args, args.id]).then(data => data),
+  createUser: (args, req) => queryDB(req, "insert into users SET ?", args).then(data => data),
+  deleteUser: (args, req) => queryDB(req, "delete from users where id = ?", [args.id]).then(data => data),
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+httpServer = require('http').createServer(app);
+httpServer.listen('4000');
+console.log('Running a GraphQL API server at localhost:4000/graphql');
