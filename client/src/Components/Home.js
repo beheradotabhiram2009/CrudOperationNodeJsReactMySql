675e2497df369bc8eda4ddba2d2c9f7f38b4d374
Abhiram Behera <behera.abhiram2009@gmail.com>
2023-09-28 15:57:26 +0530

    Update Home.js

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,17 +1,19 @@
-import {React} from "react";
+import React from "react";
 import { useMutation, useQuery } from '@apollo/client';
 import { GET_USERS, DELETE_USER } from '../Queries';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table, Button} from 'react-bootstrap';
 import { useNavigate , Link} from 'react-router-dom';
+import { blobToBase64 } from "../Base64";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
 	const navigate = useNavigate();
 
-	const {data, error:getUsersError, refetch} = useQuery(GET_USERS);
+	const users = useQuery(GET_USERS);
 	const [ deleteUser ] = useMutation(DELETE_USER,);
 
-	refetch();//refetch the query when redirecting
+	users.refetch();//refetch the query when redirecting
 	
 	const handleDelete = async (userid) => {
 		const resp = window.confirm("Are you sure to delete this User?");
@@ -21,14 +23,13 @@ function Home() {
 			navigate('/');
 		}catch(error){alert(error)}
 	}
-	function setUser(id, name, email, job_title) {
-        localStorage.setItem('id', id);//string value is stored
-        localStorage.setItem('name', name);
-        localStorage.setItem('email', email);
-		localStorage.setItem('job_title', job_title);
-    }
+	function setUserId(uid){
+		localStorage.setItem('id', uid);//string value is stored
+	}
+	
 	return (
-		<div style={{marginLeft:'10rem', marginRight:'10em'}}>
+		<div style={{marginLeft:'5rem', marginRight:'5em'}}>
+			 {/* <p>{JSON.stringify(users.data, null, 2)}</p> */}
 			<Table striped bordered hover size="sm">
 				<thead>
 					<tr>
@@ -36,23 +37,28 @@ function Home() {
 						<th>Name</th>
 						<th>JobTitle</th>
 						<th>EMail</th>
+						<th>Joining Date</th>
+						<th>Photo</th>
 						<th>Change</th>
 						<th>Remove</th>
 					</tr>
 				</thead>
 				<tbody>
-					{getUsersError ? alert(getUsersError) : null}
-					{data ? data.getUsers.map((user, index) => (
+					{users.error ? alert(users.error) : null}
+					{users.data?.getUsers.map((user, index) => (
 						<tr key={index}>
 							<td>{user.id}</td>
 							<td>{user.name}</td>
 							<td>{user.job_title}</td>
 							<td>{user.email}</td>
+							<td>{new Date(user.joining_date).toDateString()}</td>
+					    	<td><img src= {'data:image/jpeg;base64,'+blobToBase64(user.content)} width={50} height={50} alt='' /></td>
 							<td>
 								<Link to={`/edit`}>
-									<Button onClick={(e) =>
-									setUser(user.id, user.name, user.email, user.job_title)
-									}variant="info" size="sm">Update</Button>
+									<Button onClick={(e) =>{
+									//alert(user.id)
+									setUserId(user.id)
+									}}variant="info" size="sm">Update</Button>
 								</Link>
 							</td>
 							<td>
@@ -61,7 +67,7 @@ function Home() {
 								}variant="danger" size="sm">Delete</Button>
 							</td>
 						</tr>
-					)):null}
+					))}
 				</tbody>
 			</Table>
 			<Link  to='/create'>
