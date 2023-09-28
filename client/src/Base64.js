2824d3f2f78802cfb9869a1a4621cda7b7665815
Abhiram Behera <behera.abhiram2009@gmail.com>
2023-09-28 15:58:39 +0530

    Create Base64.js

---
+++ client/src/Base64.js
@@ -0,0 +1,16 @@
+//to displa the image
+export const  blobToBase64 = (value) =>{
+    const byteNumbers = value?.data;
+    let byteChars = '';
+    for (let i = 0; i < byteNumbers?.length; i++) {
+        byteChars += String.fromCharCode(byteNumbers[i]);
+    }
+    //alert(byteChars);
+    return atob(byteChars);
+}; 
+
+//to set the date
+export const toDateStr=(dt)=>{
+    const m = dt.getMonth()+1;
+    return (dt.getFullYear() + '-' + m + '-' + dt.getDate());
+}
